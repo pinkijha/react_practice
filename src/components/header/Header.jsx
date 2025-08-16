4f8be514911dfbf47326e896778bf4fdc2e43338
Pinki Jha <pinkijha1310@gmail.com>
2025-08-16 13:28:34 +0530

    feat: add the clickebale home icon on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button, Input } from "antd";
+import { House} from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const { Search } = Input;
   return (
     <div className="flex justify-between items-center bg-green-100 p-4 shadow-md">
       <div>
-        <h1 className="text-2xl font-bold">Header Component</h1>
+        <h1 className="text-3xl text-[#008000] font-bold"><NavLink to="/"><House/></NavLink></h1>
       </div>
       <div>
         <Search
