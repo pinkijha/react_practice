824f265bb94206e6ad63449728d5bf1d1dd33226
Pinki Jha <pinkijha1310@gmail.com>
2025-09-08 13:04:28 +0530

    fix: add the buttons of increment and decrement

--- src/pages/ProgressBar.jsx
+++ src/pages/ProgressBar.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const ProgressBar = () => {
   return (
     <div>
-      <h1>Progree Bar</h1>
+      <div>
+        <div className='w-96 h-6 bg-gray-200 rounded-full m-2'>
+          <div className='h-6 bg-blue-500 rounded-full' style={{width: '50%'}}></div>
+        </div>
+      </div>
+      <div className='flex gap-2 m-2'>        
+      <button className='bg-gray-200 rounded-sm p-1 border cursor-pointer'>-10%</button>
+      <button className='bg-gray-200 rounded-sm p-1 border cursor-pointer'>+10%</button>
+      </div>
     </div>
   )
 }
